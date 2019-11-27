05874bd80f72ca494032356ef0f152dc952cb6f7
Djancoeg69 <fatku.rachman29@gmail.com>
2019-11-27 12:34:02 +0700

    first commit

---
+++ src/component/Cart.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Cart extends Component {
+    render() {
+        return (
+            <div>
+                Ini Halaman cart
+            </div>
+        );
+    }
+}
+
+export default Cart;
